23cd5f72a73dac5769f77b9dec3c3ab853bff429
Philipp Wischnewski <phil-wisch@web.de>
2024-08-22 23:18:10 +0200

    SQL Setup

---
+++ setup.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS game_rankings;
+CREATE DATABASE game_rankings;
+
+
+CREATE TABLE genre (
+    id VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE game (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    rating int check( rating between 0 and 10)
+);
+
+CREATE TABLE game_genre_rankings (
+    game_id INTEGER NOT NULL,
+    genre_id varchar(255) NOT NULL,
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
+);

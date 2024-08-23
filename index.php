c52484c758f48e9ec21e816fb4de99fe41bbc435
Philipp Wischnewski <phil-wisch@web.de>
2024-08-23 23:59:17 +0200

    db test

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo phpversion();
+$host = "localhost";
+$port = "5432";
+$dbname = "game_rankings";
+$dbuser = "postgres";
+
+// Connection string without a password
+$conn_string = "host=$host port=$port dbname=$dbname user=$dbuser";
+
+$dbconn = pg_connect($conn_string);
+
+if (!$dbconn) {
+    echo "Error: Unable to connect to the database.\n";
+} else {
+    echo "Connected to the database successfully!\n";
+}
+?>
